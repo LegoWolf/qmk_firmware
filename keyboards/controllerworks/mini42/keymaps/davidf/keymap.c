5fcdb169841bb34fc1372e9d56c3a0475b632625
falkner <falkner@martica.org>
2023-03-06 23:24:47 -0700

    mini42: Updated copyright notices.

--- keyboards/controllerworks/mini42/keymaps/davidf/keymap.c
+++ keyboards/controllerworks/mini42/keymaps/davidf/keymap.c
@@ -1,6 +1,7 @@
 /*
 Copyright 2019 @foostan
 Copyright 2020 Drashna Jaelre <@drashna>
+Copyright 2023 David Falkner <falkner@martica.org>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
