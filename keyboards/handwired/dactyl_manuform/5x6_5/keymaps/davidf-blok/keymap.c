b3f8354561d5fdf1bb1d9c7afa8e74836f8d309e
LegoWolf <glynwolf@gmail.com>
2023-03-24 23:44:00 -0600

    Updated davidf keymap to work with QMK v20.1.
    Removed deprecated keycode identifiers.
    Updated keymap to reflect the VIA layout I've been using for a while.
    Removed USER codes that are no longer necessary.
    Added new USER codes for shortcuts to accented characters requiring a deadkey.

--- keyboards/handwired/dactyl_manuform/5x6_5/keymaps/davidf-blok/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6_5/keymaps/davidf-blok/keymap.c
@@ -16,11 +16,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include QMK_KEYBOARD_H
+#include <keymap_canadian_multilingual.h>
 
 typedef enum {
     LAYER_BASE,
     LAYER_SUPER,
-    LAYER_FUNC
+    LAYER_FUNC,
+    LAYER_NAV,
+    LAYER_GAME
 } custom_layers_t;
 
 typedef enum {
@@ -30,72 +33,70 @@ typedef enum {
 
 typedef enum {
 #ifdef VIA_ENABLE
-    KC_OSMODE = USER00,
+    KC_OSMODE = QK_USER_0,
 #else
     KC_OSMODE = SAFE_RANGE,
 #endif
-    KC_VERTICAL_BAR,
-    KC_MISSION_CONTROL,
-    KC_LAUNCHPAD,
-    KC_GRV_REMAP,
-    KC_GRV_REMAP_S,
-    KC_NUBS_REMAP
+    KC_VERTICAL_BAR,        // QK_USER_1
+    KC_A_CIRCUMFLEX,        // QK_USER_2
+    KC_E_CIRCUMFLEX,        // QK_USER_3
+    KC_E_DIAERESIS,         // QK_USER_4
+    KC_I_CIRCUMFLEX,        // QK_USER_5
+    KC_I_DIAERESIS,         // QK_USER_6
+    KC_O_CIRCUMFLEX,        // QK_USER_7
+    KC_U_CIRCUMFLEX,        // QK_USER_8
+    KC_U_DIAERESIS,         // QK_USER_9
+    KC_Y_DIAERESIS          // QK_USER_10
 } custom_keycodes_t;
 
 #define KC_VBAR KC_VERTICAL_BAR 
-#define KC_MSCT KC_MISSION_CONTROL 
-#define KC_LCPD KC_LAUNCHPAD
 #define MAX_LAYERS (LAYER_NUM + 1)
 
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [LAYER_BASE] = LAYOUT_5x6_5(
-  //,----------------+------------+----------------+----------------+----------------+-----------------.  ,-----------------+------------------+------------------+----------------+-----------------+----------------.
-      KC_ESCAPE,       KC_1,        KC_2,            KC_3,            KC_4,            KC_5,                KC_6,             KC_7,              KC_8,              KC_9,            KC_0,             KC_MINUS,
-  //|----------------|------------|----------------|----------------|----------------|-----------------|  |-----------------|------------------|------------------|----------------|-----------------|----------------|
-      MO(LAYER_SUPER), KC_Q,        KC_W,            KC_E,            KC_R,            KC_T,                KC_Y,             KC_U,              KC_I,              KC_O,            KC_P,             MO(LAYER_SUPER),
-  //|----------------|------------|----------------|----------------|----------------|-----------------|  |-----------------|------------------|------------------|----------------|-----------------|----------------|
-      KC_EQL,          KC_A,        KC_S,            KC_D,            KC_F,            KC_G,                KC_H,             KC_J,              KC_K,              KC_L,            KC_SCOLON,        KC_QUOT,
-  //|----------------|------------|----------------|----------------|----------------|-----------------|  |-----------------|------------------|------------------|----------------|-----------------|----------------|
-      KC_LSHIFT,       KC_Z,        KC_X,            KC_C,            KC_V,            KC_B,                KC_N,             KC_M,              KC_COMMA,          KC_DOT,          KC_SLASH,         KC_RSHIFT,
-  //`----------------+------------+----------------|----------------|----------------|-----------------|  |-----------------|------------------|------------------|----------------+-----------------+----------------,
-                       KC_NUBS_REMAP, KC_BSLS,       KC_BSPC,         LCTL_T(KC_TAB),  KC_LALT,             KC_RALT,          KC_ENT,            KC_SPC,            KC_LBRC,         KC_RBRC,
-  //                 `------------|----------------|----------------|----------------|-----------------|  |-----------------|------------------|------------------|----------------|-----------------,
-                                                                      KC_LGUI,         KC_LCTRL,            KC_RCTRL,         MO(LAYER_FUNC)
-  //                                                                `----------------+-----------------,  `-----------------+------------------,                                                    
-  ),
+    [LAYER_BASE] = LAYOUT_5x6_5(
+        KC_ESC,          KC_1,        KC_2,            KC_3,            KC_4,            KC_5,                KC_6,             KC_7,              KC_8,              KC_9,            KC_0,             KC_MINUS,
+        MO(LAYER_SUPER), KC_Q,        KC_W,            KC_E,            KC_R,            KC_T,                KC_Y,             KC_U,              KC_I,              KC_O,            KC_P,             MO(LAYER_SUPER),
+        KC_EQL,          KC_A,        KC_S,            KC_D,            KC_F,            KC_G,                KC_H,             KC_J,              KC_K,              KC_L,            KC_SCLN,          KC_QUOT,
+        KC_LSFT,         KC_Z,        KC_X,            KC_C,            KC_V,            KC_B,                KC_N,             KC_M,              KC_COMMA,          KC_DOT,          KC_SLASH,         KC_RSFT,
+                         KC_NUBS,     KC_BSLS,   LT(LAYER_NAV,KC_BSPC), LCTL_T(KC_TAB),  KC_LALT,             KC_RALT,          KC_ENT,            KC_SPC,            KC_LBRC,         KC_RBRC,
+                                                                        KC_LGUI,         KC_LCTL,             KC_RCTL,          MO(LAYER_FUNC)                                             
+    ),
 
-  [LAYER_SUPER] = LAYOUT_5x6_5(
-  //,----------------+------------+----------------+----------------+----------------+-----------------.  ,-----------------+------------------+------------------+----------------+-----------------+----------------.
-      _______,         KC_BRID,     KC_BRIU,         KC_MSCT,         KC_LCPD,         _______,             XXXXXXX,          KC_MRWD,           KC_MPLY,           KC_MFFD,         KC_MUTE,          _______,
-  //|----------------|------------|----------------|----------------|----------------|-----------------|  |-----------------|------------------|------------------|----------------|-----------------|----------------|
-      _______,         KC_PSLS,     KC_P7,           KC_P8,           KC_P9,           KC_PMNS,             KC_INS,           KC_PGUP,           KC_UP,             KC_HOME,         KC_VBAR,          _______,
-  //|----------------|------------|----------------|----------------|----------------|-----------------|  |-----------------|------------------|------------------|----------------|-----------------|----------------|
-      KC_PCMM,         KC_PAST,     KC_P4,           KC_P5,           KC_P6,           KC_PPLS,             KC_DEL,           KC_LEFT,           KC_DOWN,           KC_RGHT,         KC_GRV_REMAP_S,   _______,
-  //|----------------|------------|----------------|----------------|----------------|-----------------|  |-----------------|------------------|------------------|----------------|-----------------|----------------|
-      KC_CAPSLOCK,     KC_PEQL,     KC_P1,           KC_P2,           KC_P3,           KC_PENT,             _______,          KC_PGDN,           _______,           KC_END,          KC_GRV_REMAP,     _______,
-  //`----------------+------------+----------------|----------------|----------------|-----------------|  |-----------------|------------------|------------------|----------------+-----------------+----------------,
-                       KC_P0,       KC_PDOT,         _______,         _______,         _______,             _______,          _______,           _______,           KC_VOLD,         KC_VOLU,
-  //                 `------------|----------------|----------------|----------------|-----------------|  |-----------------|------------------|------------------|----------------|-----------------,
-                                                                      KC_APP,          _______,             _______,          _______
-  //                                                                `----------------+-----------------,  `-----------------+------------------,                                                    
-  ),
+    [LAYER_SUPER] = LAYOUT_5x6_5(
+        XXXXXXX,         XXXXXXX,     XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,             XXXXXXX,          XXXXXXX,           XXXXXXX,           XXXXXXX,         XXXXXXX,          XXXXXXX,
+        XXXXXXX,         KC_BSLS,     XXXXXXX,         KC_QUOT,         XXXXXXX,         KC_INS,              KC_DEL,           KC_NUBS,           KC_GRV,            KC_LBRC,         KC_VBAR,          XXXXXXX,
+        XXXXXXX,         KC_1,        KC_2,            KC_3,            KC_4,            KC_5,                KC_6,             KC_7,              KC_8,              KC_9,            KC_0,             XXXXXXX,
+        KC_CAPS,         XXXXXXX,     XXXXXXX,         KC_RBRC,         XXXXXXX,         XXXXXXX,             KC_APP,           XXXXXXX,           XXXXXXX,           XXXXXXX,         XXXXXXX,          KC_CAPS,
+                         XXXXXXX,     XXXXXXX,         TG(LAYER_GAME),  XXXXXXX,         _______,             _______,          KC_OSMODE,         _______,           XXXXXXX,         XXXXXXX,
+                                                                        _______,         _______,             _______,          _______                                                  
+    ),
 
-  [LAYER_FUNC] = LAYOUT_5x6_5(
-  //,----------------+------------+----------------+----------------+----------------+-----------------.  ,-----------------+------------------+------------------+----------------+-----------------+----------------.
-      KC_NUM,          KC_F1,       KC_F2,           KC_F3,           KC_F4,           KC_F5,               KC_F6,            KC_F7,             KC_F8,             KC_F9,           KC_F10,           KC_PAUS,
-  //|----------------|------------|----------------|----------------|----------------|-----------------|  |-----------------|------------------|------------------|----------------|-----------------|----------------|
-      _______,         KC_F11,      KC_F12,          KC_F13,          KC_F14,          KC_F15,              KC_F16,           KC_F17,            KC_F18,            KC_F19,          KC_F20,           _______,
-  //|----------------|------------|----------------|----------------|----------------|-----------------|  |-----------------|------------------|------------------|----------------|-----------------|----------------|
-      _______,         _______,     _______,         _______,         _______,         _______,             _______,          _______,           _______,           _______,         _______,          _______,
-  //|----------------|------------|----------------|----------------|----------------|-----------------|  |-----------------|------------------|------------------|----------------|-----------------|----------------|
-      _______,         _______,     _______,         _______,         _______,         _______,             _______,          _______,           _______,           _______,         _______,          _______,
-  //`----------------+------------+----------------|----------------|----------------|-----------------|  |-----------------|------------------|------------------|----------------+-----------------+----------------,
-                       KC_PSCR,     KC_SLCK,         _______,         _______,         _______,             _______,          _______,           _______,           KC_WH_U,         KC_WH_D,
-  //                 `------------|----------------|----------------|----------------|-----------------|  |-----------------|------------------|------------------|----------------|-----------------,
-                                                                      KC_OSMODE,       _______,             _______,          _______
-  //                                                                `----------------+-----------------,  `-----------------+------------------,                                                    
-  )
+    [LAYER_FUNC] = LAYOUT_5x6_5(
+        XXXXXXX,         XXXXXXX,     XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,             XXXXXXX,          KC_BRID,           KC_BRIU,           KC_MCTL,         KC_LPAD,          XXXXXXX,
+        XXXXXXX,         KC_F11,      KC_F12,          KC_F13,          KC_F14,          KC_F15,              KC_F16,           KC_F17,            KC_F18,            KC_F19,          KC_F20,           XXXXXXX,
+        XXXXXXX,         KC_F1,       KC_F2,           KC_F3,           KC_F4,           KC_F5,               KC_F6,            KC_F7,             KC_F8,             KC_F9,           KC_F10,           XXXXXXX,
+        KC_LSFT,         KC_PAUS,     KC_PSCR,         KC_NUM,          KC_SCRL,         XXXXXXX,             KC_MPRV,          KC_MNXT,           KC_VOLD,           KC_VOLU,         KC_MUTE,          KC_RSFT,
+                         XXXXXXX,     XXXXXXX,         XXXXXXX,         XXXXXXX,         _______,             _______,          XXXXXXX,           XXXXXXX,           KC_MPLY,         XXXXXXX,
+                                                                        _______,         _______,             _______,          _______                                                  
+    ),
+
+    [LAYER_NAV] = LAYOUT_5x6_5(
+        XXXXXXX,         XXXXXXX,     XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,             XXXXXXX,          XXXXXXX,           XXXXXXX,           XXXXXXX,         XXXXXXX,          XXXXXXX,
+        XXXXXXX,         XXXXXXX,     KC_HOME,         KC_UP,           KC_END,          KC_PGUP,             KC_PGUP,          KC_HOME,           KC_UP,             KC_END,          XXXXXXX,          XXXXXXX,
+        XXXXXXX,         XXXXXXX,     KC_LEFT,         KC_DOWN,         KC_RGHT,         KC_PGDN,             KC_PGDN,          KC_LEFT,           KC_DOWN,           KC_RGHT,         XXXXXXX,          XXXXXXX,
+        _______,         XXXXXXX,     KC_LGUI,         KC_LCTL,         KC_LALT,         XXXXXXX,             XXXXXXX,          KC_LGUI,           KC_RCTL,           KC_RALT,         XXXXXXX,          _______,
+                         XXXXXXX,     XXXXXXX,         XXXXXXX,         XXXXXXX,         _______,             _______,          XXXXXXX,           XXXXXXX,           KC_WH_U,         KC_WH_D,
+                                                                        _______,         _______,             _______,          _______                                                  
+    ),
+
+    [LAYER_GAME] = LAYOUT_5x6_5(
+        KC_ESC,          KC_1,        KC_2,            KC_3,            KC_4,            KC_5,                XXXXXXX,          XXXXXXX,           XXXXXXX,           XXXXXXX,         XXXXXXX,          TG(LAYER_GAME),
+        KC_TAB,          KC_T,        KC_Q,            KC_W,            KC_E,            KC_R,                KC_PSLS,          KC_P7,             KC_P8,             KC_P9,           KC_PPLS,          XXXXXXX,
+        KC_LSFT,         KC_G,        KC_A,            KC_S,            KC_D,            KC_F,                KC_P0,            KC_P4,             KC_P5,             KC_P6,           KC_PMNS,          XXXXXXX,
+        XXXXXXX,         KC_B,        KC_Z,            KC_X,            KC_C,            KC_V,                KC_PAST,          KC_P1,             KC_P2,             KC_P3,           KC_PCMM,          XXXXXXX,
+                         XXXXXXX,     XXXXXXX,         KC_ENT,          KC_SPC,          KC_LALT,             KC_RALT,          KC_PENT,           KC_PEQL,           KC_PDOT,         XXXXXXX,
+                                                                        KC_LGUI,         KC_LCTL,             KC_RCTL,          XXXXXXX                                                  
+    )
 };
 
 static os_modes_t os_mode = WIN;
@@ -106,7 +107,28 @@ static void emit_key_event(uint16_t keycode, keyrecord_t *record)
         register_code16(keycode);
     } else {
         unregister_code16(keycode);
-    }  
+    }
+}
+
+static bool swap_key_event(uint16_t keycode, keyrecord_t *record)
+{
+    if (os_mode == MAC) {
+        emit_key_event(keycode, record);
+        return false;
+    }
+    return true;
+}
+
+static bool send_deadkey_event(uint16_t deadkeycode, uint16_t keycode, keyrecord_t *record)
+{
+    if (record->event.pressed) {
+        uint8_t mods = get_mods() & (MOD_MASK_SHIFT | MOD_BIT(KC_RALT));
+        del_mods(mods);
+        tap_code16(deadkeycode);
+        add_mods(mods);
+        tap_code16(keycode);
+    }
+    return false;
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -145,22 +167,25 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
               }
             return false;
 
-        case KC_NUBS_REMAP:
-            // Mac and Windows swap the placement of NUBS and GRAVE under CSA keyboard layout.
-            emit_key_event((os_mode == WIN) ? KC_NONUS_BSLASH : KC_GRAVE, record);
-            return true;
+        // Single-stroke versions of keys that normally require deadkey composition.
+        case KC_A_CIRCUMFLEX: return send_deadkey_event(CA_CIRC, KC_A, record);
+        case KC_E_CIRCUMFLEX: return send_deadkey_event(CA_CIRC, KC_E, record);
+        case KC_E_DIAERESIS: return send_deadkey_event(CA_DIAE, KC_E, record);
+        case KC_I_CIRCUMFLEX: return send_deadkey_event(CA_CIRC, KC_I, record);
+        case KC_I_DIAERESIS: return send_deadkey_event(CA_DIAE, KC_I, record);
+        case KC_O_CIRCUMFLEX: return send_deadkey_event(CA_CIRC, KC_O, record);
+        case KC_U_CIRCUMFLEX: return send_deadkey_event(CA_CIRC, KC_U, record);
+        case KC_U_DIAERESIS: return send_deadkey_event(CA_DIAE, KC_U, record);
+        case KC_Y_DIAERESIS: return send_deadkey_event(CA_DIAE, KC_Y, record);
 
-        case KC_GRV_REMAP:
-        case KC_GRV_REMAP_S:
-        	{
-	            // Mac and Windows swap the placement of NUBS and GRAVE under CSA keyboard layout.
-	        	uint16_t kc = (os_mode == WIN) ? KC_GRAVE : KC_NONUS_BSLASH;
-	            emit_key_event((keycode == KC_GRV_REMAP_S) ? LSFT(kc) : kc, record);
-	            return true;
-	        }
+        // Mac and Windows swap the placement of NUBS and GRAVE under CSA keyboard layout.
+        case KC_NONUS_BACKSLASH: return swap_key_event(CA_SLSH, record);
+        case S(KC_NONUS_BACKSLASH): return swap_key_event(S(CA_SLSH), record);
+        case CA_SLSH: return swap_key_event(KC_NONUS_BACKSLASH, record);
+        case S(CA_SLSH): return swap_key_event(S(KC_NONUS_BACKSLASH), record);
 
         case KC_PAUSE:
-        case KC_SCROLLLOCK:
+        case KC_SCROLL_LOCK:
             if (os_mode == MAC) {
                 // Suppress scroll lock on Mac. It just gets confused.
                 return false;
