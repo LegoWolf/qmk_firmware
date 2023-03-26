ce62e3d0cba28e529e31b30484f579b8efc10966
LegoWolf <glynwolf@gmail.com>
2023-03-25 23:47:20 -0600

    dactyl-davidf: Changed À keycode to KC_NUHS.

--- keyboards/handwired/dactyl_manuform/5x6_5/keymaps/davidf-blok/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6_5/keymaps/davidf-blok/keymap.c
@@ -58,13 +58,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         MO(LAYER_SUPER), KC_Q,        KC_W,            KC_E,            KC_R,            KC_T,                KC_Y,             KC_U,              KC_I,              KC_O,            KC_P,             MO(LAYER_SUPER),
         KC_EQL,          KC_A,        KC_S,            KC_D,            KC_F,            KC_G,                KC_H,             KC_J,              KC_K,              KC_L,            KC_SCLN,          KC_QUOT,
         KC_LSFT,         KC_Z,        KC_X,            KC_C,            KC_V,            KC_B,                KC_N,             KC_M,              KC_COMMA,          KC_DOT,          KC_SLASH,         KC_RSFT,
-                         KC_NUBS,     KC_BSLS,   LT(LAYER_NAV,KC_BSPC), LCTL_T(KC_TAB),  KC_LALT,             KC_RALT,          KC_ENT,            KC_SPC,            KC_LBRC,         KC_RBRC,
+                         KC_NUBS,     KC_NUHS,   LT(LAYER_NAV,KC_BSPC), LCTL_T(KC_TAB),  KC_LALT,             KC_RALT,          KC_ENT,            KC_SPC,            KC_LBRC,         KC_RBRC,
                                                                         KC_LGUI,         KC_LCTL,             KC_RCTL,          MO(LAYER_FUNC)                                             
     ),
 
     [LAYER_SUPER] = LAYOUT_5x6_5(
         XXXXXXX,         XXXXXXX,     XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,             XXXXXXX,          XXXXXXX,           XXXXXXX,           XXXXXXX,         XXXXXXX,          XXXXXXX,
-        XXXXXXX,         KC_BSLS,     XXXXXXX,         KC_QUOT,         XXXXXXX,         KC_INS,              KC_DEL,           KC_NUBS,           KC_GRV,            KC_LBRC,         KC_VBAR,          XXXXXXX,
+        XXXXXXX,         KC_NUHS,     XXXXXXX,         KC_QUOT,         XXXXXXX,         KC_INS,              KC_DEL,           KC_NUBS,           KC_GRV,            KC_LBRC,         KC_VBAR,          XXXXXXX,
         XXXXXXX,         KC_1,        KC_2,            KC_3,            KC_4,            KC_5,                KC_6,             KC_7,              KC_8,              KC_9,            KC_0,             XXXXXXX,
         KC_CAPS,         XXXXXXX,     XXXXXXX,         KC_RBRC,         XXXXXXX,         XXXXXXX,             KC_APP,           XXXXXXX,           XXXXXXX,           XXXXXXX,         XXXXXXX,          KC_CAPS,
                          XXXXXXX,     XXXXXXX,         TG(LAYER_GAME),  XXXXXXX,         _______,             _______,          KC_OSMODE,         _______,           XXXXXXX,         XXXXXXX,
